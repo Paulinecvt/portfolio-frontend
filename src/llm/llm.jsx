a444dd5b3e1dbb836929203e9c2b11149c7e0f84
Pauline Cvt <paulinecvt2021@gmail.com>
2024-10-09 18:32:19 +0200

    LLM Page 50%

--- src/llm/llm.jsx
+++ src/llm/llm.jsx
@@ -1,9 +1,137 @@
 export const LLM = () => (
+  <div>
+    <p className="title"> My work as a LLM Trainer :</p>
+    <p>
+      I'm actually working on this section to propose an interesting approach of
+      my work. Thanks for your interest, don't hesitate to contact me for
+      further information waiting for the page to be finished.
+    </p>
+    <br />
+    <br />
 
-    <div>
-        <p className="title"> My work as a LLM Trainer :</p>
-        <p>I'm actually working on this section to propose an interesting approach of my work. Thanks for your interest, don't hesitate to contact me for further information waiting for the page to be finished.
-        </p>
+    <div className="flex flex-col mb-4">
+      <h3 className="title text-left uppercase">
+        AI, LLM, what is it and why training them ?
+      </h3>
+      <br />
+      <p className="flex flex-col justify-start text-justify">
+        Over the past hundred years, we have witnessed an explosion of
+        technological innovations that have profoundly transformed our daily
+        lives. From washing machines to smartphones, and from the web to
+        connected devices, these inventions have integrated so seamlessly into
+        our lives that they have become indispensable. As sociologist Anthony
+        Giddens pointed out, modernity is characterized by an “acceleration of
+        technological change”, which redefines social interactions and reshapes
+        economic and political structures. Today, artificial intelligence (AI)
+        is at the forefront of this shift, with an impact as profound as that of
+        past industrial revolutions.
+      </p>
+      <br />
+      <div className="flex flex-row justify-between">
+        <div className="w-3/5 flex flex-col justify-start text-justify mr-6">
+          <p>
+            AI, particularly large language models (LLMs), have the ability to
+            analyze vast amounts of data in seconds, providing instant
+            responses. They can act as versatile experts, offering advice across
+            numerous subjects, or as empathetic assistants responding to our
+            everyday concerns. However, their effectiveness hinges on a crucial
+            capability: understanding not just the content of our requests, but
+            also the tone and context behind them.
+          </p>
+          <br />
+          <p>
+            LLMs, as their name suggests, are trained on massive linguistic
+            datasets to learn how to decode our questions and respond
+            appropriately. But this learning process is far from autonomous.
+            Behind these AI models are teams of specialists working tirelessly
+            to refine their outputs. These experts establish strict rules, or
+            guidelines, to ensure the models meet safety and reliability
+            standards. For example, it’s vital that AI relies on scientifically
+            validated sources and does not generate answers when lacking
+            reliable information. Moreover, they must be able to detect
+            malicious intent from users and avoid undesirable behaviors, such as
+            disinformation or hate speech.
+          </p>
+          <br />
+          <p className="flex flex-col justify-start text-justify">
+            {" "}
+            The societal impact of these innovations is considerable. They
+            reshape our relationship with knowledge and information, creating
+            both opportunities for increased automation and challenges regarding
+            ethics and safety. In this age of instant information, how AI models
+            are trained and supervised becomes a critical issue to prevent
+            potential risks and ensure responsible use of technology. Behind the
+            scenes, experts ensure that these systems evolve within a secure
+            framework, providing solutions that protect both users and society
+            as a whole.
+          </p>
+        </div>
+        <div className="w-2/6 flex justify-end">
+          <a href="https://i.ibb.co/VDNN4P8/Image-convertio.jpg">
+            <img
+              src="https://i.ibb.co/VDNN4P8/Image-convertio.jpg"
+              alt="illustration"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+        <br/>
+        <br/>
+
+        <div className="flex flex-row mb-4">
+   
+    <div className="flex flex-col justify-start text-justify">
+        <h3 className="title uppercase text-left">Diversity, inclusivity and translation challenges</h3>
+        <p>My native language, French, is not known for its inclusivity regarding gender. From our earliest 
+            years of learning, it is common for the masculine form to take precedence over the 
+            feminine in expression. Traditional French rules, such as proximity agreement and the 
+            feminization of professions, have been challenged by the Académie Française over the 
+            years. Today, in light of contemporary concerns, inclusive writing and the feminization 
+            of professions spark debate in both the media and governmental bodies. Various groups 
+            are organizing to explore ways to adapt our language to current usage without losing 
+            complexity or adding reading difficulties for populations already faced with the many 
+            rules governing our grammar. </p>
+        <br/>
+        <div className="flex flex-row justify-between text-justify">
+            <div className="flex flex-col justify-end w-3/5 mr-6">
+        <p className="flex flex-col justify-start text-justify">By learning English -US- and exploring its usage, I discovered the diversity of 
+            pronouns and their importance for representing identities. By studying the work of 
+            Howard S. Becker, it becomes clear that our use of language has profound implications 
+            for the perception and representation of identities, particularly those related to gender. 
+            The choice of words and labels we use shapes not only our understanding of individuals 
+            but also influences how these individuals identify and interact with society, including 
+            the technologies it offers. </p>
+        <br/>
+        <p className="flex flex-col justify-start text-justify"> This issue is all the more crucial in the age of artificial intelligence. As these systems 
+            learn and adapt to our languages, it is imperative to ensure that they are trained with 
+            inclusive and representative language to avoid perpetuating existing biases and 
+            stereotypes in our society. By integrating inclusive linguistic practices, we have the 
+            opportunity to shape artificial intelligences that meet the needs of a diverse population 
+            and contribute to a more equitable and just society. This remains a challenge in French.</p>
+        <br/>
+        <p className="flex flex-col justify-start text-justify">Technological inclusivity allows a wide range of users to feel represented, encouraging 
+            them to turn to language models to solve their problems. Without fearing judgment from 
+            controversial sources that might question their identity or render them invisible, 
+            French-speaking users also need an inclusive language adapted to the plurality of their 
+            profiles, despite traditional grammar rules that can often be arbitrary.</p>
+            </div>
+    <div className="w-2/6 flex justify-start"> 
+        <a href="https://i.ibb.co/ctSMw1V/Image-2.jpg">
+            <img
+              src="https://i.ibb.co/ctSMw1V/Image-2.jpg"
+              alt="illustration"
+            />
+        </a>
+    </div>
+    <div/>
     </div>
 
+</div>
+  </div>
+
+
+
+
+  </div>
 );
